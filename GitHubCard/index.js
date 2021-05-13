f4de40e8b0988aa03ddf91f648dbe099e1d864d7
Joe Hebert <raphaelhebert18@gmail.com>
2021-05-13 12:05:15 +0700

    added MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,8 @@
     https://api.github.com/users/<your name>
 */
 
+axios.get("https://api.github.com/users/RaphaelHebert")
+  
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -16,7 +18,7 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-
+  .then(item => document.querySelector(".cards").appendChild(gitCardCreator(item.data)))
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -28,8 +30,18 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [ "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+];
 
+followersArray.forEach(user => {
+  axios
+  .get(`https://api.github.com/users/${user}`)
+  .then(item => document.querySelector(".cards").appendChild(gitCardCreator(item.data)))
+})
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -50,6 +62,47 @@ const followersArray = [];
     </div>
 */
 
+function gitCardCreator({ avatar_url,  name,  login,  loc, url, followers, following}){
+  // create the elements
+  const card = document.createElement("div");
+  const pict = document.createElement("img");
+  const info = document.createElement("div");
+  const title = document.createElement("h3");
+  const text = document.createElement("p");
+  const loc1 = document.createElement("p");
+  const profile = document.createElement("p");
+  const fing = document.createElement("p");
+  const fers = document.createElement("p");
+  const bio = document.createElement("p");
+  const page = document.createElement("a");
+  // organize the elts
+  card.appendChild(pict);
+  card.appendChild(info);
+  info.appendChild(title);
+  info.appendChild(text);
+  info.appendChild(loc1);
+  info.appendChild(profile);
+  profile.appendChild(page);
+  info.appendChild(fers);
+  info.appendChild(fing);
+  info.appendChild(bio);
+  // add style
+  card.classList.add("card");
+  info.classList.add("card-info");
+  title.classList.add("name");
+  text.classList.add("username");
+  // add content
+  pict.src = avatar_url
+  pict.alt = "user's profile"
+  title.textContent = name
+  loc1.textContent = loc
+  text.textContent = login
+  fers.textContent = followers
+  fing.textContent = following
+
+  // return the elt
+  return card
+}
 /*
   List of LS Instructors Github username's:
     tetondan
